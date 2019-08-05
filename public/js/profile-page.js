5b717210b4c341a616d6500c79482cd353c7cba7
Dupe Fadina <maafadina@hotmail.com>
2019-08-05 15:01:56 -0700

    08/05 3:00pm
    Matched the ids in the HTML and JS for the profile page.

--- public/js/profile-page.js
+++ public/js/profile-page.js
@@ -4,13 +4,19 @@
 */
 $(document).ready(function() {
     $('.parallax').parallax();
-    //references to elements on profile page
-    var favRecipesList = $("#favorite-recipes-names");
-    var madeRecipesList = $("#created-recipes-names");
-    var buttonSubmitRecipe = $("#submit-recipe");
+    //references to unordered lists that will display recipes.
+    var favRecipesList = $("#favorite-recipes");
+    var madeRecipesList = $("#made-recipes");
+   //references to form elements
     var recipeName = $("#recipe-name");
-    var recipeIngridents = $("#recipe-ingridents");
+    var recipeIngredients = $("#recipe-ingredients");
     var recipeSteps = $("#recipe-steps");
+    //references to icons and buttons that trigger events
+    var buttonSubmitRecipe = $("#submit-recipe");
+    var iRevealMadeRecipe = $("#reveal-made-recipes");
+    var iRevealFavRecipe = $("#reveal-fav-recipes");
+
+    
     // The API object contains methods for each kind of request we'll make
     var API = {
         addRecipes: function(recipe) {
@@ -35,30 +41,33 @@ $(document).ready(function() {
     var showRecipes = function(columnName, cardList) {
         API.getRecipes().then(function(data) {
           var recipes = data.map(function(recipe) {
-            var a = $("<a>")
-              .text(recipe.columnName)
-              .attr("href", "/recipe/" + recipe.id);
-      
+            if (cardList == madeRecipesList) {
+              var i = $("<i>")
+                .text("cake")
+                .attr({
+                  class: "material-icons"
+                })
+            }
+            else {
+              var i = $("<i>")
+                .text("favorite")
+                .attr({
+                  class: "material-icons"
+                })
+            }
             var li = $("<li>")
-              .attr({
-                class: "list-group-item",
-                "data-id": recipe.id
+              .text(recipe.columnName)
+              attr({
+                class: "collection-item"
               })
-              .append(a);
+              .append(i)
+              return li;
+            });
       
-            return li;
-          });
-      
-          cardList.empty();
-          cardList.append(recipes);
+          cardList.empty();// I AM RIGHT HERE !
+          cardList.append(recipes);// I AM RIGHT HERE !
         });
     };
-
-    //display the favorite recipes
-    showRecipes(favorite, favRecipesList);
-    //display the made recipes
-    showRecipes(made, madeRecipesList); //Is "made" one of the  column names for the author or recipe tables?
-
 
     // handleFormSubmit is called whenever we add(submit) a new recipe
     // Save the new example to the db and refresh the list
@@ -67,15 +76,9 @@ $(document).ready(function() {
       
         var recipe = {
           name: recipeName.val().trim(),
-          ingridents: recipeIngridents.val().trim(),
+          ingredients: recipeIngredients.val().trim(),
           steps: recipeSteps.val().trim()
         };
-      
-        /*if (!(example.text && example.description)) {
-          alert("You must enter an example text and description!");
-          return;
-        }*/
-      
         API.addRecipes(recipe).then(function() {
             //show updated displays of favorite recipes and made recipes.
             showRecipes(favorite, favRecipesList);
@@ -84,11 +87,17 @@ $(document).ready(function() {
         
         //clear the form after posting new recipe to the server.
         recipeName.val("");
-        recipeIngridents.val("");
+        recipeIngredients.val("");
         recipeSteps.val("");
     };
 
     // Add event listener to the submit button
     buttonSubmitRecipe.on("click", handleFormSubmit);
-    
+    //Add event listener for icons to reveal recipes
+    iRevealFavRecipe.on("click", showRecipes(favorite, favRecipeList));
+    iRevealMadeRecipe.on("click", showRecipes(made, madeRecipesList));
+
+    //MAKE SURE THAT THE DATABASE HAS COLUMN NAMES `favorite` and `made`......
+    //WORK ON THE ELEMENTS FOR THE LISTS favorite and made. DONE
+    //WORK ON THE SUMBIT FORM AND ITS ELEMENTS. DONE
 });
