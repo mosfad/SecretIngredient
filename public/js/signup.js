cda78274c3ea36cab6af7714f6a6f6de5d4766c2
tiffany rivas <triva002@ucr.edu>
2019-08-08 21:32:28 -0700

    deleted some errors

--- public/js/signup.js
+++ public/js/signup.js
@@ -13,31 +13,6 @@ function signup(event){
         password:$("#password").val().trim()
     };
 
-    username = $("#username").val().trim();
-  password = $("#password").val().trim();
-  contact = $("#contact").val().trim();
-  destination = $("#destination").val().trim();
-
-  submitform();
-});
-
-// Force the user to input their information.
-// function submitform() {
-//   if (username === "" || password === "" || contact === "" || destination === "") {
-//     $('#formModal').modal('show');
-//   }
-//   else {
-//     signup();
-
-
-//     // Clear form after submitting 
-//     $("#username").val("");
-//     $("#contact").val("");
-//     $("#destination").val("");
-//     $("#login").hide();
-//   }
-// }
-
     $.ajax("/api/signup", {
         type: "POST",
         data: newAuthor
