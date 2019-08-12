26b49a12d4adcc313c91c0ed1c5712570f919908
MesayBeyene <mesibella@gmail.com>
2019-08-11 18:35:43 -0700

    Last Commit Sunday 6:35pm

--- config/config.js
+++ config/config.js
@@ -1,24 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "aurora726",
-    "database": "recipe_db",
-    "host": "localhost",
-    "port": 3306,
-    "dialect": "mysql",
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "port": 3306,
-    "dialect": "mysql",
-  
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mysql"
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
-}
+};
