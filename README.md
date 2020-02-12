ba190540ca0a90c0810fdf27ba51656694983bc6
Dupe Fadina <maafadina@hotmail.com>
2020-02-12 13:48:51 -0800

    Adding appropriate content to the README file.

--- README.md
+++ README.md
@@ -1,9 +1,51 @@
-# Project-2-
-I Intalled express, mysql, mysql2, sequelize, inquirer.
-something 
+# SecretIngredient
 
-https://api.edamam.com/search?q=chicken&app_id=5ae54cff&app_key=2fecaffd34d74567cc8225f296bcbec1
+SecretIngredient is a web app designed to help users create, share, and save their favorite recipes. This app was created to encourage users to cook meals as
+eating home-cooked meals has been associated with healthier lifesytles.
 
-<!-- Dupe: Profile -->
-<!-- Mesi: Database and creating list in table column -->
-<!-- Ricardo/Me: html and css  -->
+## Overview
+
+SecretIngredient allows all users to search for all the recipes availabe on the platform. The full stack app provides authenticated accounts for users who want to register. Only authenticated users can create and save recipes.
+
+### Technologies used
+
+jQuery
+Node.js
+Express.js
+MySQL/Sequelize(ORM)
+Passport.js(Authentication)
+AWS S3(Data storage)
+
+## Instructions
+
+### [Demo](https://drive.google.com/file/d/1j0IBdSy5WOqXpVvezPKjMLdDRoG_COk1/view)
+
+1. To only view all the recipes on the platform, click on 'Search' navigation bar link.
+2. To create an account, scroll to the bottom of the home page and click on the 'SIGN-UP' button. Alternatively, click on the 'create account' link on the sign up page.
+3. After successfully logging in, you can manage your account by
+
+- clicking on 'ADD NEW RECIPE' modal to create a new recipe.
+- clicking on 'X' button to delete favorite recipes
+- clicking on recipe name link to edit or save a recipe
+
+4. To edit a recipe, you must have created that recipe.
+5. Use this test account to enjoy the features available to authenticated users:
+   - test username: _jroland@gmail.com_
+   - test password: _@123_456_
+
+- Notes
+  - Please click on the demo provided to help you navigate this app.
+  - Only the user who created a recipe is allowed to modify it.
+  - Future goals include adding real-time chat capabilities to connect users, so that they can share and improve the recipes.
+
+## Deployed App
+
+https://secretingredient.herokuapp.com/
+
+## Contact us about SecretIngredient
+
+maafadina@gmail.com
+
+## Contributors
+
+Ricardo (Team Leader), Dupe, Mesi, Tiffany
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,69 +1,4 @@
-//var db = require("../models");
-
 module.exports = function(app) {
-  /*
-  app.get("/", function(req,res){
-    res.render("index");
-  })
-  // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Author.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-
-  // Load example page and pass in an example by id
-  app.get("/signin", function(req, res) {
-    // db.Author.findOne({ where: { email: req.body.email,
-    //   password: req.body.email}}).then(function(dbAuthor) {
-    //   res.render("recipe", {
-    //     author: dbAuthor
-    //   });
-    // });
-    res.render("recipe");
-  });
-
-  app.get("/profile-page", function(req, res) {
-    // db.Author.findOne({ where: { email: req.body.email,
-    //   password: req.body.email}}).then(function(dbAuthor) {
-    //   res.render("recipe", {
-    //     author: dbAuthor
-    //   });
-    // });
-    res.render("recipe");
-  });
-
-  app.get("/account-setting", function(req, res) {
-    // db.Author.findOne({ where: { email: req.body.email,
-    //   password: req.body.email}}).then(function(dbAuthor) {
-    //   res.render("recipe", {
-    //     author: dbAuthor
-    //   });
-    // });
-    res.render("recipe");
-  });
-
-
-  app.get("/profile-page", function(req, res) {
-    // db.Author.findOne({ where: { email: req.body.email,
-    //   password: req.body.email}}).then(function(dbAuthor) {
-    //   res.render("recipe", {
-    //     author: dbAuthor
-    //   });
-    // });
-    res.render("recipe");
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
-*/
-
   // Requiring our custom middleware for checking if a user is logged in
   var isAuthenticated = require("../config/middleware/isAuthenticated");
   var path = require("path");
@@ -74,7 +9,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      return res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
@@ -113,34 +48,6 @@ module.exports = function(app) {
   app.get("/searchAll", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/searchAll.html"));
   });
-  // app.get("/author", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/author.html"));
-  // });
-
-  // // cms route loads cms.html
-  // app.get("/cms", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/cms.html"));
-  // });
-
-  // // blog route loads blog.html
-  // app.get("/account-settings", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/account-settings.html"));
-  // });
-
-  // // authors route loads author-manager.html
-  // app.get("/recipe-list", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/recipe-list.html"));
-  // });
-
-  // // authors route loads author-manager.html
-  // app.get("/profile-page", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/profile-page.html"));
-  // });
-  // authors route loads author-manager.html
-  /*
-  app.get("/sign-in", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/sign-in.html"));
-  });*/
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
